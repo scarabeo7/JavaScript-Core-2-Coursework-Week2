bff392e62b3f71aa6d31ae51b6d7991620fd39a3
Chizim <garrohat@yahoo.co.uk>
2021-01-16 20:42:58 +0000

    created image folder and added a header to page

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,23 +1,27 @@
+let content = document.getElementById("content")
+let header = document.createElement("h1");
+header.innerText = "Book list";
+content.appendChild(header);
+
 function readingList(books) {
-  // Write your code here...
+    // Write your code here...
 }
 
-const books = [
-  {
-    title: "The Design of Everyday Things",
-    author: "Don Norman",
-    alreadyRead: false
-  },
-  {
-    title: "The Most Human Human",
-    author: "Brian Christian",
-    alreadyRead: true
-  },
-  {
-    title: "The Pragmatic Programmer",
-    author: "Andrew Hunt",
-    alreadyRead: true
-  }
+const books = [{
+        title: "The Design of Everyday Things",
+        author: "Don Norman",
+        alreadyRead: false
+    },
+    {
+        title: "The Most Human Human",
+        author: "Brian Christian",
+        alreadyRead: true
+    },
+    {
+        title: "The Pragmatic Programmer",
+        author: "Andrew Hunt",
+        alreadyRead: true
+    }
 ];
 
 readingList(books);
