8915550b0f53a23808588d95e190c025c6563b15
Chizim Chinuru <garrohat@yahoo.co.uk>
2021-01-16 23:26:49 +0000

    done reading list

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,10 +1,41 @@
-let content = document.getElementById("content")
+// Adding header to page
+let content = document.getElementById("content");
 let header = document.createElement("h1");
 header.innerText = "Book list";
 content.appendChild(header);
+header.style.margin = "30px";
 
 function readingList(books) {
-    // Write your code here...
+  // Append image src to each book passed from the object array
+  books[0].src = "img/image-1.jpg";
+  books[1].src = "img/image-2.jpg";
+  books[2].src = "img/image-3.jpg";
+
+  let content = document.getElementById("content");
+  let unorderedList = document.createElement("ul");
+  content.appendChild(unorderedList);
+
+  books.forEach((item) => {
+    // p element and append textContent to display title and author
+    let li = document.createElement("li");
+    let p = document.createElement("p");
+    let img = document.createElement("img");
+    p.textContent = `${item.title} - ${item.author}`;
+    img.src = item.src;
+   
+    li.appendChild(p);
+    li.appendChild(img);
+    unorderedList.appendChild(li);
+    unorderedList.style.listStyle = "none";
+    unorderedList.style.display = "flex";
+    li.style.margin = "2em";
+
+    if (item.alreadyRead) {
+      li.style.backgroundColor = "green";
+    } else {
+      li.style.backgroundColor = "red";
+    }
+  });
 }
 
 const books = [{
