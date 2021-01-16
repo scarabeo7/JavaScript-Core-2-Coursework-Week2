84db7556a420ecfe45238adb3d2772b53f212d91
Chizim <garrohat@yahoo.co.uk>
2021-01-16 20:42:18 +0000

    done shopping list

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,14 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(arrayOfList) {
+    let content = document.getElementById("content");
+    let unorderedList = document.createElement("ul");
+    let shoppingItem;
+    content.appendChild(unorderedList);
+    arrayOfList.forEach(item => {
+        shoppingItem = document.createElement("li");
+        shoppingItem.innerText = item;
+        unorderedList.appendChild(shoppingItem);
+    })
+
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
