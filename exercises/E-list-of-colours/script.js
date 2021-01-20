a738779f31033d832a2caf12240c647c6ec55abb
Chizim Chinuru <garrohat@yahoo.co.uk>
2021-01-20 09:51:37 +0000

    halfway done list of colors

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,7 +1,27 @@
-function listOfColours(colours) {
-  // Write your code here...
-}
+const colours = ["Red", "Blue", "Green", "Yellow", "Pink", "Brown"];
 
-const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
+function listOfColours(colours) {
+  let div = document.getElementById("content");
+  let select = document.createElement("select");
+  div.appendChild(select);
+  let p = document.createElement("p");
+  div.appendChild(p);
+
+  colours.forEach(element => {
+    let option = document.createElement("option");
+    select.appendChild(option);
+    option.innerText = element;
+  });
+  
+  //TODO Each `<option>` should have a 'click' event listener to update the contents and colour of
+  // the `<p>` with the selected colour.
+  let option = document.querySelectorAll("option");
+  option.forEach((item) => {
+    console.log(item);
+    item.addEventListener("click", function () {
+  
+    });
+  });
+};
 
 listOfColours(colours);
