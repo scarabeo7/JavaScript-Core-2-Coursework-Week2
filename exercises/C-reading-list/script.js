161b3bc1f7cca3e9ee6f5804d79e3877925badfc
Chizim <garrohat@yahoo.co.uk>
2021-01-22 00:45:04 +0000

    refactored code

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -6,36 +6,36 @@ content.appendChild(header);
 header.style.margin = "30px";
 
 function readingList(books) {
-  // Append image src to each book passed from the object array
-  books[0].src = "img/image-1.jpg";
-  books[1].src = "img/image-2.jpg";
-  books[2].src = "img/image-3.jpg";
+    // Append image src to each book passed from the object array
+    books[0].src = "img/image-1.jpg";
+    books[1].src = "img/image-2.jpg";
+    books[2].src = "img/image-3.jpg";
 
-  let content = document.getElementById("content");
-  let unorderedList = document.createElement("ul");
-  content.appendChild(unorderedList);
+    let content = document.getElementById("content");
+    let unorderedList = document.createElement("ul");
+    content.appendChild(unorderedList);
 
-  books.forEach((item) => {
-    // p element and append textContent to display title and author
-    let li = document.createElement("li");
-    let p = document.createElement("p");
-    let img = document.createElement("img");
-    p.textContent = `${item.title} - ${item.author}`;
-    img.src = item.src;
-   
-    li.appendChild(p);
-    li.appendChild(img);
-    unorderedList.appendChild(li);
-    unorderedList.style.listStyle = "none";
-    unorderedList.style.display = "flex";
-    li.style.margin = "2em";
+    books.forEach(book => {
+        // p element and append textContent to display title and author
+        let li = document.createElement("li");
+        let p = document.createElement("p");
+        let img = document.createElement("img");
+        p.textContent = `${book.title} - ${book.author}`;
+        img.src = book.src;
 
-    if (item.alreadyRead) {
-      li.style.backgroundColor = "green";
-    } else {
-      li.style.backgroundColor = "red";
-    }
-  });
+        li.appendChild(p);
+        li.appendChild(img);
+        unorderedList.appendChild(li);
+        unorderedList.style.listStyle = "none";
+        unorderedList.style.display = "flex";
+        li.style.margin = "2em";
+
+        if (book.alreadyRead) {
+            li.style.backgroundColor = "green";
+        } else {
+            li.style.backgroundColor = "red";
+        }
+    });
 }
 
 const books = [{
