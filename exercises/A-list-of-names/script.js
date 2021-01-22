9d8090a9ee57450ef4f35aa746052b2793ea0551
Chizim <garrohat@yahoo.co.uk>
2021-01-22 00:44:49 +0000

    Refactored code

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,20 +1,20 @@
-function listOfNames(arrayOfPeople) {
-    let content = document.querySelector("#content");
-    let header1, header2;
-    arrayOfPeople.forEach(element => {
-        header1 = document.createElement("h1");
-        header2 = document.createElement("h2");
-        header1.innerText = element.name;
-        content.appendChild(header1);
-        header2.innerText = element.job;
-        content.appendChild(header2);
-    });
-}
-
 let people = [
     { name: "Chris", job: "Teacher" },
     { name: "Joanna", job: "Student" },
     { name: "Boris", job: "Prime Minister" },
 ];
+
+function listOfNames(arrayOfPeople) {
+    let content = document.querySelector("#content");
+    let header1, header2;
+    arrayOfPeople.forEach(people => {
+        header1 = document.createElement("h1");
+        header2 = document.createElement("h2");
+        header1.innerText = people.name;
+        content.appendChild(header1);
+        header2.innerText = people.job;
+        content.appendChild(header2);
+    });
+};
 
 listOfNames(people);
